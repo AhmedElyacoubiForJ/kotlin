9631211ce0f0c97b1a37a28d9215fa8bac5c9bb2
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-07-19 23:19:10 +0200

    Convert from one data type to another

--- src/main/kotlin/basics/ConvertFromOneDataTypeToAnother.kt
+++ src/main/kotlin/basics/ConvertFromOneDataTypeToAnother.kt
@@ -1,5 +1,13 @@
 package basics
 
 fun main() {
+    val number = "10"
+    println(10 + number.toInt())
+
+    println(10.toString() + number)
+
+    val d = 4.5
+    println(d)
+    println(d.toInt())
 
 }
