d68506815359a8ceb89fe4ba3f4dbba95ce4d1e0
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-07-20 20:16:45 +0200

    intro to array but not the best way to do this

---
+++ src/main/kotlin/arraysandlists/Intro.kt
@@ -0,0 +1,6 @@
+package arraysandlists
+
+fun main() {
+    // is not the best way to store names
+    val names: String = "Jamila, Jamal"
+}
--- src/main/kotlin/conditionals/WhenBooleansAreNull.kt
+++ src/main/kotlin/conditionals/WhenBooleansAreNull.kt
@@ -2,7 +2,7 @@ package conditionals
 
 fun main() {
     val isAdult: Boolean? = null
-    
+
     // when boolean is null we must perform this check
     if (isAdult == true) {
         println("not null and true")
