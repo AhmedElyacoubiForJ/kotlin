05f1d949cd2ea73e881cfeb2835cf76fd905cbb3
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-07-20 12:47:51 +0200

    added fun example for if-else-if-else-else construct

---
+++ src/main/kotlin/conditionals/ElseIf.kt
@@ -0,0 +1,18 @@
+package conditionals
+
+fun main() {
+    val n1 = 30
+    val n2 = 20
+
+    if (n1 >= n2 || n1 <= 30) {
+        println(":)")
+    } else if (n1 == 100) {
+
+    } else if (n1 == 1000) {
+        
+    } else {
+        println(":(")
+    }
+
+    println("some code")
+}
