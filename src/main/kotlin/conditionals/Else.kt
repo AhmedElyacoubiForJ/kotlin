f87a59ca58594480f7c55704d6cc835eef4c7d92
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-07-20 12:38:04 +0200

    added else to if statement

---
+++ src/main/kotlin/conditionals/Else.kt
@@ -0,0 +1,22 @@
+package conditionals
+
+fun main() {
+    val n1 = 30
+    val n2 = 20
+    println("if begin")
+    if (n1 > n2) {
+        println(":)")
+    } else {
+        println(":(")
+    }
+    println("if end")
+
+    println("if begin")
+    if (n1 < n2) {
+        println(":)")
+    } else {
+        println(":(")
+    }
+    println("if end")
+    println("some code")
+}
