282ec101ecb21bf1c280df8a5a41a90f63ca740b
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-07-20 09:43:35 +0200

    added func for boolean naming convention

--- src/main/kotlin/conditionals/Booleans.kt
+++ src/main/kotlin/conditionals/Booleans.kt
@@ -4,7 +4,7 @@ fun main() {
     val isAdult = false
     val isMale  = true
     val orderCompleted: Boolean? = null
-    
+
     println(isAdult)
     println(isMale)
     println(orderCompleted)
---
+++ src/main/kotlin/conditionals/NamingBooleans.kt
@@ -0,0 +1,14 @@
+package conditionals
+
+fun main() {
+    val isAdult = false
+    val isMale  = true
+    // ask question for naming convention
+    // like hasSlept isCompleted
+    //val orderCompleted: Boolean? = null
+    val isOrderCompleted: Boolean? = null
+
+    println(isAdult)
+    println(isMale)
+    println(isOrderCompleted)
+}
