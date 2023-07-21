1189c88c01088d5741808e197354932536b33f9b
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-07-21 11:53:57 +0200

    + fun exam. how to use for-loops for lists and arrays

---
+++ src/main/kotlin/loops/ForLoop.kt
@@ -0,0 +1,21 @@
+package loops
+
+fun main() {
+    val names = listOf("mary", "ali", "alex")
+    val numbers = intArrayOf(1, 2, 3, 4, 5)
+
+    for (name in names) {
+        val n = name.replaceFirstChar {
+                c: Char -> c.uppercase()
+        }
+        println(n)
+    }
+
+    names.forEach{
+        println(it)
+    }
+    
+    println()
+
+    for (number in numbers) println(number)
+}
