e65eb729198ff79f3fe57e538f723ce6e7cc7cf3
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-07-21 13:19:49 +0200

    + fun examp. iteration through a list using forEach method

---
+++ src/main/kotlin/loops/forEach.kt
@@ -0,0 +1,14 @@
+package loops
+
+fun main() {
+    val names = listOf(
+        "Jamila",
+        "Jamal",
+        "Peter",
+        "Ahmed"
+    )
+    names.forEach {
+        println(it)
+    }
+    // for (name in names) println(name)
+}
