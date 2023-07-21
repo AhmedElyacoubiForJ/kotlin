a883e662d8454a758325be8e8991c1bdf60ac962
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-07-21 16:04:40 +0200

    + fum to demonstrate difference between while-loop & do-while-loop

---
+++ src/main/kotlin/loops/DoWhile.kt
@@ -0,0 +1,12 @@
+package loops
+
+fun main() {
+    while (false) {
+        println("hello while-loop")
+    }
+    // different between while-loop & do-while-loop
+    // is that the last one it will be executed at least one time
+    do {
+        println("hello do-while-loop")
+    } while (false)
+}
