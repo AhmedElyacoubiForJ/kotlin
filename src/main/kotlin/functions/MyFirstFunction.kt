a00063f1a964fd87894e3a61ef7db0c7a687f940
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-07-21 20:25:55 +0200

    simple fun definition & call

---
+++ src/main/kotlin/functions/MyFirstFunction.kt
@@ -0,0 +1,12 @@
+package functions
+
+fun main(): Unit {
+    // fun call
+    greet()
+}
+// Unit, this type corresponds
+// to the void type in Java (but is redundant)
+// fun definition
+fun greet(): Unit {
+    println("hello")
+}
