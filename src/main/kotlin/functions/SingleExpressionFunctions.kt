c3b705f359e83d02e5e8858a4cc280f8c97b3333
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-07-22 14:24:34 +0200

    function definition as expression

---
+++ src/main/kotlin/functions/SingleExpressionFunctions.kt
@@ -0,0 +1,12 @@
+package functions
+
+fun main() {
+    println(doubleFunAsExpression(10))
+}
+// only if the implementation has one instruction
+fun doubleFunAsExpression(number: Int): Int = number * 2
+
+//fun double2(number: Int): Int {
+//    println()
+//    return 2 * number
+//}
