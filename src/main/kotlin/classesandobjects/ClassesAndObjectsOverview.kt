b268fbc9c98a2fe66a3581a05ef790941cfd7f7c
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-07-23 11:43:26 +0200

    intro how to design class & how to create objects of it

---
+++ src/main/kotlin/classesandobjects/ClassesAndObjectsOverview.kt
@@ -0,0 +1,13 @@
+package classesandobjects
+
+fun main() {
+    // tv & phone are objects of class-type SmartDevice
+    val tv = SmartDevice()
+    val phone = SmartDevice()
+}
+
+// blueprint (construct how to design a class)
+class SmartDevice {
+    // properties. variables either w. var or val
+    // behaviours. functions
+}
