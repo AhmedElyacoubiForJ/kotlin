37138c61139f3f96a7b969e05a77f2b5faf74c10
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-07-24 12:59:04 +0200

    how to define more than one constructor to a kotlin class

--- src/main/kotlin/classesandobjects/Constructors.kt
+++ src/main/kotlin/classesandobjects/Constructors.kt
@@ -11,20 +11,31 @@ fun main() {
 
     println()
 
-    val phone = SmartDevice5 (
-        "Apple Iphone",
-        1149.00
-    )
-    println("${tv.brand} ${tv.price}")
-    tv.turnOn()
-    tv.getDeviceState()
+    val laptop = SmartDevice5()
+    laptop.brand = "Acer aspire E 15"
+    laptop.price = 399.00
+    println("${laptop.brand} ${laptop.price}")
+    laptop.turnOn()
+    laptop.getDeviceState()
+
+    println()
+
+    val laptop2 = SmartDevice5("Lenovo")
+    laptop2.price = 559.00
+    println("${laptop2.brand} ${laptop2.price}")
+    laptop2.turnOn()
+    laptop2.getDeviceState()
 }
 
 class SmartDevice5(// constructor( key word not needed
-    var brand: String,
-    var price: Double,
-    var isSwitchedOn: Boolean = false,
+    var brand: String?,
+    var price: Double?,
+    var isSwitchedOn: Boolean = false
 ) {
+    // other constructors
+    constructor(): this(brand = null, price = null)
+    constructor(brand: String) : this(brand, price = 0.0)
+
     // methods/functions
     fun getDeviceState() {
         println("$brand is on: $isSwitchedOn")
